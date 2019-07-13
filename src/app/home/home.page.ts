b3c17cca1adade1c43dc614bc98f7909a487c160
Jose Guaman <arturoguaman96@gmail.com>
2019-07-13 11:02:53 -0500

    subir

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { RequestOptions, Headers} from '@angular/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,26 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(public http: HttpClient) {}
 
+    sendPostRequest() {
+      var headers = new Headers();
+        headers.append('Access-Control-Allow-Origin' , '*');
+        headers.append('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT');
+        headers.append('Accept','application/json');
+        headers.append('content-type','application/json');
+         let options = new RequestOptions({ headers:headers});
+      let postData = {
+         "email": "admin@admin.com",
+  		  "password": "123321"
+      }
+      //var info;
+      let ruta="http://clientes.geekslatam.com/profesionales/GeeksVersion/public/api/getuser";
+      this.http.post(ruta, postData) 
+        .subscribe(data => {
+          console.log(data);
+         }, error => {
+          console.log(error);
+        });
+  }
 }
